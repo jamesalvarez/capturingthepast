c8c5bbedb5d221d202947633ec2339f953d270bb
James Alvarez <jamesa333uk@gmail.com>
2022-06-14 15:05:33 +0100

    Initial Commit

---
+++ Capturing the Past/Capturing_the_PastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capturing_the_PastApp.swift
+//  Capturing the Past
+//
+//  Created by James Alvarez on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Capturing_the_PastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capturing the Past/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Capturing the Past
+//
+//  Created by James Alvarez on 14/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
