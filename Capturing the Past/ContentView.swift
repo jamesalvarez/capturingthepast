8d9988c6e1139cbcc59403f3323695e13534897c
James Alvarez <jamesa333uk@gmail.com>
2022-06-14 15:32:32 +0100

    Basic form elements

--- Capturing the Past/ContentView.swift
+++ Capturing the Past/ContentView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var repositories = ["Repository 1", "Repository 2"]
+    @State private var selectedRepositoryIndex = 0
+    @State private var catelogueRefernce = ""
+    @State private var item = 0
+    @State private var subItem = 0
+    @State private var specialCase = 0
+    @State private var note = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Form {
+                Section {
+                    Picker(selection: $selectedRepositoryIndex, label: Text("Repository")) {
+                        ForEach(0 ..< repositories.count) {
+                            Text(self.repositories[$0])
+                        }
+                    }
+        
+                    TextField("Catalogue reference", text: $catelogueRefernce)
+                    Stepper(value: $item, in: 0...999) {
+                        Text("Item: \(item)")
+                    }
+                    Stepper(value: $subItem, in: 0...999) {
+                        Text("Sub Item: \(subItem)")
+                    }
+                    Stepper(value: $specialCase, in: 0...999) {
+                        Text("Special Case: \(specialCase)")
+                    }
+                    TextField("Note", text: $note)
+                }
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
