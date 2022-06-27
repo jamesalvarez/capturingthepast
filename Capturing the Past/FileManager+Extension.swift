fc3fccd43f6f2c904c54db4b6b92cd26b5c74a1a
James Alvarez <jamesa333uk@gmail.com>
2022-06-27 14:35:55 +0100

    File methods for saving and loading images

--- Capturing the Past/FileManager+Extension.swift
+++ Capturing the Past/FileManager+Extension.swift
@@ -17,4 +17,31 @@ extension FileManager {
     func docExist(named docName: String) -> Bool {
         fileExists(atPath: Self.docDirURL.appendingPathComponent(docName).path)
     }
+
+    func saveImage(_ id: String, image: UIImage) throws {
+        if let data = image.jpegData(compressionQuality: 0.6) {
+            let imageURL = FileManager.docDirURL.appendingPathComponent("\(id).jpg")
+            do {
+                try data.write(to: imageURL)
+            } catch {
+                throw CapturingThePastError.saveImageError
+            }
+        } else {
+            throw CapturingThePastError.saveImageError
+        }
+    }
+
+    func readImage(with id: String) throws -> UIImage {
+        let imageURL = FileManager.docDirURL.appendingPathComponent("\(id).jpg")
+        do {
+            let imageData = try Data(contentsOf: imageURL)
+            if let image = UIImage(data: imageData) {
+                return image
+            } else {
+                throw CapturingThePastError.readImageError
+            }
+        } catch {
+            throw CapturingThePastError.readImageError
+        }
+    }
 }
