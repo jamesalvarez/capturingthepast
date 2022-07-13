788d8f8396f0a7ba05d0467aed2c387f39610c9b
James Alvarez <jamesa333uk@gmail.com>
2022-07-13 09:12:39 +0100

    Storage and saving for repositories

--- Capturing the Past/CapturingThePastApp.swift
+++ Capturing the Past/CapturingThePastApp.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 @main
 struct CapturingThePastApp: App {
-    @StateObject private var store = ArchiveEntriesStore()
+    @StateObject private var archiveEntriesStore = ArchiveEntriesStore()
+    @StateObject private var repositoriesStore = RepositoriesStore()
 
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ArchiveEntriesView(archiveEntries: $store.archiveEntries) {
-                    ArchiveEntriesStore.save(archiveEntries: store.archiveEntries) { result in
+                ArchiveEntriesView(archiveEntries: $archiveEntriesStore.archiveEntries) {
+                    ArchiveEntriesStore.save(archiveEntries: archiveEntriesStore.archiveEntries) { result in
+                        if case .failure(let error) = result {
+                            fatalError(error.localizedDescription)
+                        }
+                    }
+                    RepositoriesStore.save(Repositories: repositoriesStore.repositories) { result in
                         if case .failure(let error) = result {
                             fatalError(error.localizedDescription)
                         }
@@ -28,7 +34,15 @@ struct CapturingThePastApp: App {
                     case .failure(let error):
                         fatalError(error.localizedDescription)
                     case .success(let archiveEntries):
-                        store.archiveEntries = archiveEntries
+                        archiveEntriesStore.archiveEntries = archiveEntries
+                    }
+                }
+                RepositoriesStore.load { result in
+                    switch result {
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                    case .success(let repositories):
+                        repositoriesStore.repositories = repositories
                     }
                 }
             }
---
+++ Capturing the Past/Models/RepositoriesStore.swift
@@ -0,0 +1,71 @@
+//
+//  RepositoriesStore.swift
+//  Capturing the Past
+//
+//  Created by James Alvarez on 13/07/2022.
+//
+
+import Foundation
+import SwiftUI
+
+/**
+ * Storage of archive entries, saves/loads in JSON and exports in CSV
+ */
+class RepositoriesStore: ObservableObject {
+    @Published var repositories: [Repository] = []
+
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("repositories.data")
+    }
+
+    static func load(completion: @escaping (Result<[Repository], Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileURL = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+
+                    DispatchQueue.main.async {
+                        completion(.success(Repository.initialRepositories)) //TODO: Remove or place in debug only
+                    }
+                    return
+                }
+
+                let Repositories = try JSONDecoder().decode([Repository].self, from: file.availableData)
+
+                DispatchQueue.main.async {
+                    completion(.success(Repositories))
+                }
+
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+
+    static func save(Repositories: [Repository], completion: @escaping (Result<Int, Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try JSONEncoder().encode(Repositories)
+
+                let outfile = try fileURL()
+
+                try data.write(to: outfile)
+
+                DispatchQueue.main.async {
+                    completion(.success(Repositories.count))
+                }
+
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
