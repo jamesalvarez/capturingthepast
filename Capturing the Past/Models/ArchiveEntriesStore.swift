ce61bc5a4cc2b81d7c9bb4b8bd9112dce64c1f83
James Alvarez <jamesa333uk@gmail.com>
2022-07-20 13:50:14 +0100

    Storage of CSV representation

--- Capturing the Past/Models/ArchiveEntriesStore.swift
+++ Capturing the Past/Models/ArchiveEntriesStore.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CSV
 
 /**
  * Storage of archive entries, saves/loads in JSON and exports in CSV
@@ -16,20 +17,24 @@ class ArchiveEntriesStore: ObservableObject {
 
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory,
-
                                     in: .userDomainMask,
-
                                     appropriateFor: nil,
-
                                     create: false)
             .appendingPathComponent("archive.data")
+    }
+
+    private static func csvURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("CapturingThePastLog.csv")
     }
 
     static func load(completion: @escaping (Result<[ArchiveEntry], Error>) -> Void) {
         DispatchQueue.global(qos: .background).async {
             do {
                 let fileURL = try fileURL()
-                print(fileURL)
                 guard let file = try? FileHandle(forReadingFrom: fileURL) else {
 
                     DispatchQueue.main.async {
@@ -56,10 +61,20 @@ class ArchiveEntriesStore: ObservableObject {
         DispatchQueue.global(qos: .background).async {
             do {
                 let data = try JSONEncoder().encode(archiveEntries)
-
                 let outfile = try fileURL()
-
                 try data.write(to: outfile)
+
+                let csvFile = try csvURL()
+                let csvStream = OutputStream(url: csvFile, append: false)
+                let csv = try CSVWriter(stream: csvStream!)
+
+                try csv.write(row: ["Date", "Cat Ref", "Filename", "Note"])
+                for archiveEntry in archiveEntries {
+                    try csv.write(row: ["", archiveEntry.catReference, archiveEntry.photoRefs[0], archiveEntry.note])
+                }
+
+                csv.stream.close()
+
 
                 DispatchQueue.main.async {
                     completion(.success(archiveEntries.count))
