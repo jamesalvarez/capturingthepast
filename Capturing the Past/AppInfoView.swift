d05f3f93310c1ffdd94a9cbeb81e13710ce26b4c
James Alvarez <jamesa333uk@gmail.com>
2022-07-24 20:34:34 +0100

    App Info page content

--- Capturing the Past/AppInfoView.swift
+++ Capturing the Past/AppInfoView.swift
@@ -10,6 +10,29 @@ import SwiftUI
 struct AppInfoView: View {
     @EnvironmentObject var archiveEntriesStore: ArchiveEntriesStore
     var body: some View {
-        Text("Info")
+        ScrollView {
+            Form {
+                Section("Resources") {
+                    Text(.init("[Website](https://blogs.sussex.ac.uk/capturing-the-past/about-us/)"))
+                    Text(.init("[Instructions](https://blogs.sussex.ac.uk/capturing-the-past/2021/11/04/hello-world/)"))
+                }
+                Section("Capture Log") {
+                    Text("A log file called CapturingThePast.csv is saved in your device's Documents folder and contains a list of all captures. Delete the log to reset it, or rename it to preserve it and start a fresh one.")
+                }
+                Section("Latest Captures") {
+                    List {
+                        ForEach(archiveEntriesStore.archiveEntries.suffix(min(5, archiveEntriesStore.archiveEntries.count)), id: \.id) { archiveEntry in
+                            Text(archiveEntry.photoRef).font(.system(size: 12))
+                        }
+                    }
+                }
+                Section("Acknowledgements") {
+                    Text(.init("Capturing the Past is a [Sussex Humanities Lab](https://www.sussex.ac.uk/research/centres/sussex-humanities-lab/) project funded by the [Arts and Humanities Research Council](https://ahrc.ukri.org/)"))
+                }
+            }
+            .frame(height: UIScreen.main.bounds.height)
+            .navigationTitle("Information")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
